efcdb200e2a4e81013f63624aa2c5b0847a393b2
raj <raj@aureusanalytics.com>
2019-06-03 16:18:04 +0530

    Fill ERC721Enumberable

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -343,7 +343,7 @@ contract ERC721Enumerable is ERC165, ERC721 {
      * @return uint256 token ID at the given index of the tokens list owned by the requested address
      */
     function tokenOfOwnerByIndex(address owner, uint256 index) public view returns (uint256) {
-        require(index < balanceOf(owner));
+        require(index < balanceOf(owner), "ERC721Enumerable: owner does not have tokens at this index");
         return _ownedTokens[owner][index];
     }
 
@@ -362,7 +362,7 @@ contract ERC721Enumerable is ERC165, ERC721 {
      * @return uint256 token ID at the given index of the tokens list
      */
     function tokenByIndex(uint256 index) public view returns (uint256) {
-        require(index < totalSupply());
+        require(index < totalSupply(), "ERC721Enumerable: no tokens at this index");
         return _allTokens[index];
     }
 
