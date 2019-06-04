a7522a0c4bc5db2dbaea7dc41ba87c5f36656d7e
raj <raj@aureusanalytics.com>
2019-06-04 15:27:39 +0530

    Fix deployment script

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -2,8 +2,7 @@
 const SquareVerifier = artifacts.require("./Verifier.sol");
 const SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SquareVerifier).then(() => {
-    deployer.deploy(SolnSquareVerifier, SquareVerifier.address);
-  });
+module.exports = async (deployer) => {
+  await deployer.deploy(SquareVerifier);
+  await deployer.deploy(SolnSquareVerifier, SquareVerifier.address);
 };
